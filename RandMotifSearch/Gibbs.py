51f5908da0d094d83379617b735731f97737e2fb
ThomasCZhang <112586835+ThomasCZhang@users.noreply.github.com>
2023-01-27 17:35:03 -0500

    Finished Gibbs.py

--- RandMotifSearch/Gibbs.py
+++ RandMotifSearch/Gibbs.py
@@ -2,11 +2,60 @@ import os
 from glob import glob
 from RandomizedMotifSearch import *
 
+def main():
+    dirpath = os.path.join(os.path.dirname(__file__),
+                           "Files\Inputs\Gibbs")
+    filepaths = glob(dirpath + "\\data*.txt")
+    for path in filepaths:
+        k, t, N, Dna = ReadTestFilesGibbsSampler(path)
+        BestMotifs = GibbsSampler(Dna, k, t, N)
+
+    AnswerPath = os.path.join(os.path.dirname(__file__), "GibbsAnswer.txt")
+    with open(AnswerPath, "w") as f:
+        for Line in BestMotifs:
+            f.write(Line)
+            f.write("\n")
+
+def ReadTestFilesGibbsSampler(FilePath: str) -> tuple[int, int, int, list[str]]:
+    """
+    ReadTestFilesGibbsSampler: Reads test files for Gibbs Sampler.
+    Line 1: k, t, N
+    Line 2 to the end: The Dna strands.
+
+    Input:
+        FilePath (str): Path to the test file.
+
+    Output:
+        k (int): the number of characters in the KMer
+        
+        t (int): the number of dna strings
+        
+        N (int): the number of time 
+    """
+    with open(FilePath) as f:
+        Dna = []
+        for index, line in enumerate(f):
+            line = line.strip()
+            if index == 0:
+                k, t, N = [int(i) for i in line.split(" ")]
+            else:
+                Dna.extend(line.split(" "))
+    return k, t, N, Dna
 
 def GibbsSampler(Dna: list[str], k: int, t: int, N: int) -> list[str]:
     """
-    Input: Integers k, t, and N, followed by a collection of strings Dna.
-    Output: The strings BestMotifs resulting from running GibbsSampler(Dna, k, t, N) with 20 random starts. Remember to use pseudocounts!
+    Performs the Gibbs sampling to find the best motifs from a collection of DNA sequences.
+    Input:
+        Dna (list[str]): Collection of DNA sequences.
+        
+        k (int): number of characters in a K_mer.
+        
+        t (int): number of DNA.
+        
+        N (int): number of times a motif is replaced in one run of GibbsSampler from a random set of start motifs.
+
+    Output: 
+        BestMotif (list[str]): the best motifs.
     """
     NumIterations = 20
     BestMotifs = ["" for i in range(t)]
@@ -16,25 +65,37 @@ def GibbsSampler(Dna: list[str], k: int, t: int, N: int) -> list[str]:
         NewScore = Score(Motifs)
         if NewScore < BestScore:
             BestMotifs = Motifs
+            BestScore = NewScore
     return BestMotifs
+
 
 def GibbsSamplerOneIteration(Dna: list[str], k: int, t: int, N: int) -> list[str]:
     """
-    Finds the best motif reachable from a single random starting collection of motifs from Dna using Gibbs Sampling.\n
-    Input:\n
-    Dna (list[str]): List of strings. Each string represents a separate Dna strand.
-    k (int): the number of bases in each motif.
-    t (int): the number of Dna strands.
-    N (int): the number of times to run the Gibbs iteration.
+    Finds the best motif reachable from a single random starting collection of motifs from Dna using Gibbs Sampling.
+
+    Input:
+
+        Dna (list[str]): List of strings. Each string represents a separate Dna strand.
+    
+        k (int): the number of bases in each motif.
+    
+        t (int): the number of Dna strands.
+    
+        N (int): the number of times to run the Gibbs iteration.
+
     Output:
-    BestMotifs (list[str]): A list of strings, each string contains the best motif from each Dna strand as determined
-    by Gibbs sampling.
+        BestMotifs (list[str]): A list of strings, each string contains the best motif from each Dna strand as determined
+        by Gibbs sampling.
     """
     BestMotifs = ChooseRandomMotifs(Dna, k)
     for i in range(N):
-        ind = random.randrange(k) # The index of the motif that will be ignored when making the profile.
+        ind = random.randrange(t) # The index of the motif that will be ignored when making the profile.
         Profile = CreateProfile([*BestMotifs[:ind], *BestMotifs[ind+1:]])
-        ChooseKMer(Dna[ind], k, Profile)
+        NewKMer = ChooseKMer(Dna[ind], k, Profile)
+        NewMotifs = [*BestMotifs[:ind], NewKMer, *BestMotifs[ind+1:]]
+        if Score(NewMotifs) < Score(BestMotifs):
+            BestMotifs = NewMotifs
+    return BestMotifs
 
         
 def ChooseKMer(Sequence: str, k: int, Profile: list[dict[str,int]]) -> str:
@@ -42,23 +103,30 @@ def ChooseKMer(Sequence: str, k: int, Profile: list[dict[str,int]]) -> str:
     Chooses which Kmer to put back to the best motif list based on Gibbs sampling.
     """
     Probabilities = KMerProbabilitiesOfStrand(Sequence, k, Profile)
+    KMerStartIndex = ChooseKMerIndex(Probabilities)
+    Motif = Sequence[KMerStartIndex: KMerStartIndex+k]
+    return Motif
         
-def KMerProbabilitiesOfStrand(Dna: str, k: int, Profile: list[dict[str, int]]) -> list[float])
+def KMerProbabilitiesOfStrand(Dna: str, k: int, Profile: list[dict[str, int]]) -> list[float]:
     """
     Calculates the chance of each KMer in a string occuring based on a profile.
     Returns the probabilities as a list of ints.
+
     Input:
-    Dna (str): The string being analyzed.
-    k (int): the length of the KMers.
-    profile (list[dict[str,int]]): The profile used to calculate probabilites.
+        Dna (str): The string being analyzed.
+        
+        k (int): the length of the KMers.
+    
+        profile (list[dict[str,int]]): The profile used to calculate probabilites.
+    
     Output:
-    Probabilities list[int]: A list containing the probabilites of each kmer in Dna.
+        Probabilities list[int]: A list containing the probabilites of each kmer in Dna.
     """
     NumKMers = len(Dna)-k+1
     Probabilities = [0 for i in range(NumKMers)]
     for i in range(NumKMers):
         KMer = Dna[i:i+k]
-        Probabilities[i] = ScoreKMer(Kmer, Profile)
+        Probabilities[i] = ScoreKMer(KMer, Profile)
     
     # Normalize the Probabilities so they sum to 1
     NormalizationFactor = sum(Probabilities)
@@ -74,5 +142,8 @@ def ChooseKMerIndex(Probabilities: list[float]) -> int:
     sum = 0
     for index, val in enumerate(Probabilities):
         sum += val
-        if val > DiceRoll:
+        if sum > DiceRoll:
             return index
+
+if __name__ == "__main__":
+    main()
