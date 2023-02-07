3f3c0ec6fa401284585652219bb62c2a7867edc0
ThomasCZhang <112586835+ThomasCZhang@users.noreply.github.com>
2023-02-06 23:28:10 -0500

    Starting LCS(Longest Common String).

---
+++ SeqAlignment/LCS.py
@@ -0,0 +1,6 @@
+def LongestCommonString(v: str, w: str):
+    """
+    LongestCommonString: Finds the alignment of two strings that will create the longest common string between the two
+    strings.
+    """
+    ScoreMatrix = 
