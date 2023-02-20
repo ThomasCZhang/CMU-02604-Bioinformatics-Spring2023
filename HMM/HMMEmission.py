bc68e193b8e3e0846755b0035b9e14dbda4e37fa
ThomasCZhang <112586835+ThomasCZhang@users.noreply.github.com>
2023-02-19 22:48:19 -0500

    Working HMMEmission.py

---
+++ HMM/HMMEmission.py
@@ -0,0 +1,52 @@
+import os
+from glob import glob
+
+def main():
+    dirpath = os.path.join(os.path.dirname(__file__), "files")
+    paths = glob(os.path.join(dirpath , "HMMEmission\\data*.txt"))
+    for path in paths:
+        output, alphabet, hiddenPath, state_names, hmm = ReadFiles_HMMEmission(path)
+        prob = EmissionProb(output, alphabet, hiddenPath, state_names, hmm)
+        print(prob)
+
+    anspath = os.path.join(os.path.dirname(__file__), "answer.txt")
+    with open(anspath, "w") as f:
+        f.write(str(prob))
+
+def ReadFiles_HMMEmission(path):
+    items = ["", [], "", [], {}]
+    keys = []
+    idx = 0
+    with open(path) as f:
+        for line in f:
+            if line[0:3] == "---":
+                idx += 1
+            elif idx == 0:
+                items[idx] = line.strip()
+            elif idx == 1:
+                items[idx] = line.strip().split()
+            elif idx == 2:
+                items[idx] = line.strip()
+            elif idx == 3:
+                items[idx] = line.strip().split()
+            elif idx == 4:
+                if len(keys) == 0:
+                    keys = line.strip().split()
+                else:
+                    line_0 = line.strip().split()
+                    items[idx][line_0[0]] = {}
+                    for idx0, key in enumerate(keys):
+                        items[idx][line_0[0]][key] = float(line_0[idx0 + 1])
+
+    return items[0], items[1], items[2], items[3], items[4]
+
+def EmissionProb(output: str, alphabet: list[str], hidden_path: str, states: str, transition: dict[str, dict[str, float]]) -> float:
+    prob = 1
+    for idx, curr_char in enumerate(output):
+        curr_state = hidden_path[idx]
+        prob *= transition[curr_state][curr_char]
+        curr_char = curr_char
+    return prob
+
+if __name__ == "__main__":
+    main()
