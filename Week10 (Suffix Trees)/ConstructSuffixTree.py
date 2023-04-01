be8b673a6ac3c57df08dd22bba200bda1e6dc919
ThomasCZhang <112586835+ThomasCZhang@users.noreply.github.com>
2023-04-01 11:31:02 -0400

    Converted labeling edges with strings to labeling edges with start index and length

--- Week10 (Suffix Trees)/ConstructSuffixTree.py
+++ Week10 (Suffix Trees)/ConstructSuffixTree.py
@@ -4,68 +4,83 @@ from ConstructTrie import *
 def main():
     dirname = os.path.dirname(__file__)
     filepath = os.path.join(dirname, "inputs", "SuffixTree", "dataset_876287_4.txt")
-    # filepath = os.path.join(dirname, "inputs", "SuffixTree", "input_4.txt")
+    # filepath = os.path.join(dirname, "inputs", "SuffixTree", "input_2.txt")
     with open(filepath) as f:
         word = f.readline().strip()
     suffix_tree = ConstructSuffixTree(word)
+    word_list = GetAllEdgeLabels(suffix_tree, word)
 
     answerpath = os.path.join(dirname, "answer.txt")
     with open(answerpath, 'w') as f:
-        for idx, key in enumerate(suffix_tree):
-            for idx2, edge in enumerate(suffix_tree[key]):
-                if idx != 0 or idx2 != 0:
-                    f.write(' ')
-                f.write(edge[1])
-
-
+        for i, w in enumerate(word_list):
+            if i != 0:
+                f.write(' ')
+            f.write(w)
+            
 def ConstructSuffixTree(word: str) -> dict[int, list[list[int]]]:
     """
     Constructs a suffix tree by first creating the suffix trie then compressing the edges down.
     Input:
         word: The word being used to construct a suffix tree.
     """
-    suffixes = [word[i:] for i in range(len(word))]
-    tree = {0: []}
-    for s in suffixes:
-        GrowTree(tree, s)
+    tree = {-1:[]}
+    for i in range(len(word)):
+        tree[i] = []
+    for i in range(len(word)):
+        GrowTree(tree, word, i)
     return tree
 
-def GrowTree(t: dict[int, list[list[int]]], s: str):
+def GetAllEdgeLabels(t: dict[int, list[list[int]]], word: str) -> list[str]:
     """
-    Grows a suffix tree using a string.
+    Converts all the start position, weight labels of a suffix tree into the actual string they represent.
+    Input:
+        t: The suffix tree as an adjacency dictionary (directional).
+        word: the word used to build the suffix tree.
+    """
+    word_list = []
+    for key in t:
+        for edge in t[key]:
+            word_list.append(word[edge[1]:edge[1]+edge[2]])
+    return word_list
+
+def GrowTree(t: dict[int, list[list[int]]], word: str, start_idx: int):
+    """
+    Grows a suffix tree using a suffix of a word.
     Input:
         t: the suffix tree
-        s: the string being used to grow the suffix tree
+        word: the word being used to grow the suffix tree
+        start_idx: the index corresponding to start position of the word
     """
-    s_index = 0
-    current_node = 0
-    new_node = len(t)
+    s = word[start_idx:]
+    s_index = 0 # Index along s
+    current_node = -1
+    new_node = len(t)-1
 
     while True:
-        mismatch_idx = 0
-        for i, child in enumerate(t[current_node]):
-            mismatch_idx = FindFirstMismatch(child[1], s[s_index:])
-            if mismatch_idx != 0:
+        num_matches = 0
+        for i, edge in enumerate(t[current_node]):
+            edge_label = word[edge[1]:edge[1]+edge[2]]
+            num_matches = CountMatches(edge_label, s[s_index:])
+            if num_matches != 0:
                 break
 
-        if mismatch_idx == 0: # Create branch from node
-            t[current_node].append([new_node, s[s_index:]])
+        if num_matches == 0: # Create branch from node
+            t[current_node].append([start_idx, start_idx+s_index, len(s)-s_index])
+            # t[current_node].append([new_node, s[s_index:]])
             t[new_node] = []
             break
-        elif mismatch_idx == len(child[1]):
-            current_node = child[0]
-            s_index += mismatch_idx
+        elif num_matches == edge[2]: # We reached a node that branches
+            current_node = edge[0]
+            s_index += num_matches
         else: # create branch from inside an edge
-            t[current_node].append([new_node, child[1][:mismatch_idx]]) # Add new internal node
+            t[current_node][i] =[new_node, edge[1], num_matches] # replace edge with edge to new internal node
             t[new_node] = [# Add two new children of internal node
-                [child[0], child[1][mismatch_idx:]],
-                [new_node+1, s[s_index+mismatch_idx:]]
+                [edge[0], edge[1]+num_matches, edge[2]-num_matches],
+                [start_idx, start_idx+s_index+num_matches, len(s)-s_index-num_matches]
             ]
-            t[new_node+1] = [] 
-            del t[current_node][i]
             break
 
-def FindFirstMismatch(s1: str, s2: str) -> int:
+def CountMatches(s1: str, s2: str) -> int:
     """
     Returns the index of the first mismatch between two strings.
     s1, s2: the two strings being compared.
